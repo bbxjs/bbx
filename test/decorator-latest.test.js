c1cfbfe48c9cf3bf640bcccf22cc45690991ea7d
zinkey <yaya@uloveit.com.cn>
2018-08-10 14:21:22 +0800

    fixed latest test

--- test/decorator-latest.test.js
+++ test/decorator-latest.test.js
@@ -1,6 +1,6 @@
-import { State, latest } from '../src';
+import { State, loading, latest } from '../src';
 
-describe.only('bbx latest', () => {
+describe('bbx latest', () => {
   test('latest', (done) => {
     class Data extends State {
       state = {
@@ -20,5 +20,53 @@ describe.only('bbx latest', () => {
       expect(data.state.count).toBe(1);
       done();
     }, 20);
+  });
+
+  test('latest loading', (done) => {
+    class Data extends State {
+      state = {
+        count: 'default',
+      }
+
+      @latest
+      @loading
+      async getUser(count, timer) {
+        await new Promise(r => setTimeout(r, timer));
+        this.setState({ count });
+      }
+    }
+    const data = new Data();
+    data.getUser(0, 10).then(() => {
+      expect(data.state.getUserLoading).toBe(true);
+    });
+    data.getUser(1, 200).then(() => {
+      expect(data.state.getUserLoading).toBe(false);
+      done();
+    });
+    expect(data.state.getUserLoading).toBe(true);
+  });
+
+  test('loading latest', (done) => {
+    class Data extends State {
+      state = {
+        count: 'default',
+      }
+
+      @loading
+      @latest
+      async getUser(count, timer) {
+        await new Promise(r => setTimeout(r, timer));
+        this.setState({ count });
+      }
+    }
+    const data = new Data();
+    data.getUser(0, 10).then(() => {
+      expect(data.state.getUserLoading).toBe(false);
+    });
+    data.getUser(1, 200).then(() => {
+      expect(data.state.getUserLoading).toBe(false);
+      done();
+    });
+    expect(data.state.getUserLoading).toBe(true);
   });
 });
