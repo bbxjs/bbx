7be5c0379e630cee531c0c842f062908cd825760
zinkey <yaya@uloveit.com.cn>
2018-08-17 14:22:02 +0800

    fixed connect

--- examples/func-test/pages/index.js
+++ examples/func-test/pages/index.js
@@ -21,14 +21,24 @@ class CountState extends State {
 
 const countState = new CountState();
 
+
+@connect(countState)
+class Preview extends React.Component {
+  render() {
+    return <h1>{countState.state.count}</h1>;
+  }
+}
+
 @connect(countState)
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>{countState.state.count}</h1>
+        
+        { countState.state.count }
         <button onClick={() => countState.add()}>+</button>
         <button onClick={() => countState.minus()}>-</button>
+        {countState.state.count % 2 ? <Preview /> : ''}
       </div>
     );
   }
--- src/connect.js
+++ src/connect.js
@@ -1,21 +1,30 @@
 import React from 'react';
 
-const connect = (...list) => A => class extends React.Component {
-  constructor(props) {
-    super(props);
-    list.forEach((obj) => {
-      const didStateUpdate = obj.didStateUpdate.bind(obj);
-      obj.didStateUpdate = (...args) => {
-        didStateUpdate(...args);
-        this.setState({});
-      };
-    });
-    this.state = {};
-  }
+const connect = (...list) => {
+  const array = [];
+  list.forEach((obj) => {
+    const didStateUpdate = obj.didStateUpdate.bind(obj);
+    obj.didStateUpdate = (...args) => {
+      didStateUpdate(...args);
+      array.forEach(item => item());
+    };
+  });
 
-  render() {
-    return <A {...this.props} />;
-  }
+  return A => class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.index = array.push(() => this.setState({})) - 1;
+      this.state = {};
+    }
+
+    componentWillUnmount() {
+      array.splice(this.index, 1);
+    }
+
+    render() {
+      return <A {...this.props} />;
+    }
+  };
 };
 
 export default connect;
